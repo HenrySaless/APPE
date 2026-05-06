efcb0e435b0fbebdb7abefb3e7afd852fdd008a5
Henry <wendril126@gmail.com>
2026-05-06 20:37:16 -0300

    Google cliente foi inserido

--- appe.config.js
+++ appe.config.js
@@ -1,6 +1,6 @@
 window.APPE_CONFIG = {
   functionsBaseUrl: "https://karpafjlfehxwpefpvio.supabase.co/functions/v1",
-  googleClientId: "",
+  googleClientId: "506382882703-rjbnkc5rrtph6s790rhkresjnr2ja8i1.apps.googleusercontent.com",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImthcnBhZmpsZmVoeHdwZWZwdmlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwNTEzMTksImV4cCI6MjA5MzYyNzMxOX0.iWNuh0rs990rwrh9Iaz_2Af4WHjsZmW0jaVQKsab7eU",
 };
 
