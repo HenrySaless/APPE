597e09cbc5731cabcda48476a0c9c98d753aa46c
Henrique Santos <wendril126@gmail.com>
2026-05-19 22:59:34 -0300

    subindo meu projeto local

--- appe.config.js
+++
@@ -1,4 +0,0 @@
-window.APPE_CONFIG = {
-  functionsBaseUrl: "https://karpafjlfehxwpefpvio.supabase.co/functions/v1",
-  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImthcnBhZmpsZmVoeHdwZWZwdmlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwNTEzMTksImV4cCI6MjA5MzYyNzMxOX0.iWNuh0rs990rwrh9Iaz_2Af4WHjsZmW0jaVQKsab7eU",
-};
